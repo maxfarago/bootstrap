fc3b44367c5680c84830ad77beffb03cc5e233a8
Max Farago <maxfarago@gmail.com>
2024-03-13 19:53:47 +0100

    fix: remove local logging
    isn't necessary and makes lambda management simpler

--- log.py
+++ log.py
@@ -11,12 +11,8 @@ logger.setLevel(logging.DEBUG)
 # output logs of level DEBUG and up to standard output
 stdout_handler = logging.StreamHandler(stream=sys.stdout)
 
-# output logs of ERROR level to disk
-error_handler = logging.FileHandler("error.log")
-
 # set the log levels on the handlers
 stdout_handler.setLevel(logging.DEBUG)
-error_handler.setLevel(logging.ERROR)
 
 # create a log format using Log Record attributes
 # https://docs.python.org/3/library/logging.html#logrecord-attributes
@@ -24,8 +20,6 @@ log_format = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
 # set the log format on each handler
 stdout_handler.setFormatter(log_format)
-error_handler.setFormatter(log_format)
 
 # add each handler to the Logger object
 logger.addHandler(stdout_handler)
-logger.addHandler(error_handler)
